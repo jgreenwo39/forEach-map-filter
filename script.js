2730330c6421079ad88be86b6b340e0ae84ab03d
Joe Greenwood <jgreenwo39@yahoo.com>
2022-07-16 14:34:21 -0500

    added contents to js and html files

---
+++ script.js
@@ -0,0 +1,20 @@
+// function doAllMath(a,b,mathFuncs) {
+//     for(let i = 0; i < mathFuncs.length; i++)
+//     console.log(a,b,mathFuncs[i]);
+// }
+
+// //  will these two scripts deliver the same results
+
+
+// function doAllMath(a,b,mathFuncs) {
+//     for(let func of mathFuncs) {
+//         return func(a,b);
+//     }
+// }
+
+const colors = ['teal', 'cyan', 'peach', 'purple'];
+
+function yell(val, i) {
+    const caps = val.toUpperCase();
+    console.log,(val, i)
+}
